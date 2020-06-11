e6d794f4e197ef52deb368fcefe5426355205627
Niek van der Maas <mail@niekvandermaas.nl>
2020-06-11 15:59:06 +0200

    See https://github.com/ct-Open-Source/tuya-convert/commit/50a03c83cb9461232d0718952fa485cf601adf3b

--- install_prereq.sh
+++ install_prereq.sh
@@ -10,11 +10,11 @@ PY2_DEPENDENCIES="git+https://github.com/M4dmartig4n/sslpsk.git pycrypto"
 
 if python3 -c 'import sys; exit(0) if sys.version_info.major == 3 and sys.version_info.minor < 7 else exit(1)' ;
 then
-	sudo -H pip3 install $PY3_DEPENDENCIES
-	sudo -H pip2 install $PY2_DEPENDENCIES
+	sudo -H pip3 install --upgrade $PY3_DEPENDENCIES
+	sudo -H pip2 install --upgrade $PY2_DEPENDENCIES
 else
-	sudo -H python3 -m pip install $PY3_DEPENDENCIES
-	sudo -H python2 -m pip install $PY2_DEPENDENCIES
+	sudo -H python3 -m pip install --upgrade $PY3_DEPENDENCIES
+	sudo -H python2 -m pip install --upgrade $PY2_DEPENDENCIES
 fi
 
 echo "Ready to start upgrade"
